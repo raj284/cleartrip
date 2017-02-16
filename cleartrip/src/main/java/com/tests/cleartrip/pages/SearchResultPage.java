17e3f0e14d9c0ba9dc3d50efd0444ef8b81c0b5b
Rajesh Kumar <rajeshkumar.chaudhary@practo.com>
2017-02-16 22:40:33 +0530

    Search Result page test structure

--- cleartrip/src/main/java/com/tests/cleartrip/pages/SearchResultPage.java
+++ cleartrip/src/main/java/com/tests/cleartrip/pages/SearchResultPage.java
@@ -1,5 +1,27 @@
 package com.tests.cleartrip.pages;
 
-public class SearchResultPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import com.tests.cleartrip.tests.CommonFunctions;
+import com.tests.cleartrip.tests.SearchPageTest;
+import com.tests.cleartrip.tests.Setup;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class SearchResultPage extends Setup {
+	
+    AndroidDriver<WebElement> driver;
+	private SearchPage searchpage;
+	private HomePage homepage;
+	private CommonFunctions commonFunctins;
+	
+	
+	
+	public SearchResultPage(AndroidDriver<WebElement> driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
 
 }
---
+++ cleartrip/src/test/java/com/tests/cleartrip/common/SearchResultFactory.java
@@ -0,0 +1,22 @@
+package com.tests.cleartrip.common;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class SearchResultFactory {
+	AndroidDriver<WebElement> driver;
+	
+@FindBy(xpath = "")
+protected WebElement searchResultHeader;
+
+
+@FindBy(xpath = "")
+protected WebElement searchResultDate;
+
+
+
+
+
+}
